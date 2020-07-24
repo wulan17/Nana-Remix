ef19f21812abc039f83cab578d90e67788ddb3f2
pokurt <aman@virtual.mv>
2020-07-24 20:18:47 +0500

    fixed word counter

--- nana/modules/metrics.py
+++ nana/modules/metrics.py
@@ -1,7 +1,10 @@
 # Thanks to @Athfan for a Base Plugin.
 # Go and Do a star on his repo: https://github.com/athphane/userbot
 import time
+import timeago
+
 from pyrogram import Filters
+from pyrogram.client.ext.utils import get_channel_id
 
 from nana import Command, app
 
@@ -10,8 +13,12 @@ __HELP__ = """
 This module can help you do the wordcount in the last 1000 messages in a groupchat or private chat.
 
 ──「 **Word Count** 」──
--> `wordcount`
+-> `wordcount` or `wc`
 Finds the 25 most used words in the last 1000 messages in a chat.
+
+──「 **Inactive message count** 」──
+-> `msg` or `msg <integer>`
+Finds the inactive users with last message in a chat.
 
 """
 
@@ -22,17 +29,49 @@ class Custom(dict):
         return 0
 
 
-@app.on_message(Filters.me & Filters.command("wordcount", Command))
+async def get_inactive(client, message):
+    cmd = message.command
+    start = time.time()
+    if len(cmd) > 1:
+        limit = int(" ".join(cmd[1:]))
+    else:
+        limit = 0
+
+    messages = [
+        m
+        async for member in client.iter_chat_members(
+            message.chat.id, limit=limit, filter="recent"
+        )
+        if not member.user.is_deleted
+        async for m in client.search_messages(
+            message.chat.id, limit=1, from_user=member.user.id
+        )
+    ]
+
+    delta = time.time() - start
+    messages.sort(key=lambda k: k["date"])
+
+    return "\n".join(
+        [
+            "[{}](tg://user?id={}) last [message](https://t.me/c/{}/{}) was {}".format(
+                m.from_user.first_name,
+                m.from_user.id,
+                get_channel_id(m.chat.id),
+                m.message_id,
+                timeago.format(m.date),
+            )
+            for m in messages
+        ]
+        + [f"`{int(delta * 1000)}ms`"]
+    )
+
+
+@app.on_message(Filters.me & Filters.command(["wordcount", "wc"], Command))
 async def word_count(client, message):
     await message.delete()
     words = Custom()
-    progress = await client.send_message(message.chat.id, "`Processed 0 messages...`")
-    total = 0
+    progress = await client.send_message(message.chat.id, "`Processing 1000 messages...`")
     async for msg in client.iter_history(message.chat.id, 1000):
-        total += 1
-        if total % 100 == 0:
-            await progress.edit_text(f"`Processed {total} messages...`")
-            time.sleep(0.5)
         if msg.text:
             for word in msg.text.split():
                 words[word.lower()] += 1
@@ -45,3 +84,9 @@ async def word_count(client, message):
         out += f"{i + 1}. **{words[freq[i]]}**: {freq[i]}\n"
 
     await progress.edit_text(out)
+
+
+@app.on_message(Filters.me & Filters.command("msg", Command))
+async def inactive_msg(client, message):
+    text = await get_inactive(client, message)
+    await message.edit(text)
