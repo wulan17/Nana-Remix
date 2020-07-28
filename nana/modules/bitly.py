99f202e5be9785e85d4aada3f52dffe2d31df761
pokurt <aman@virtual.mv>
2020-07-28 19:53:42 +0500

    removed bitly option for akari replacement

--- nana/modules/bitly.py
+++ nana/modules/bitly.py
@@ -1,8 +1,7 @@
-from bitlyshortener import Shortener
 from pyrogram import Filters
 from asyncio import sleep
 
-from nana import app, Command, bitly_token
+from nana import app, Command
 from nana.helpers.expand import expand_url
 
 __MODULE__ = "Link Shortner"
@@ -17,22 +16,6 @@ Shorten your url with bitly
 -> `expand (link)`
 Reply or parse arg of url to expand
 """
-
-
-@app.on_message(Filters.me & Filters.command("bitly", Command))
-async def bitly(_client, message):
-    args = message.text.split(None, 1)
-    shortener = Shortener(tokens=bitly_token, max_cache_size=8192)
-    if len(args) == 1:
-        await message.edit("Usage bitly (url)!")
-        return
-    if len(args) == 2:
-        await message.edit("Processing")
-        urls = [args[1]]
-        shortlink = shortener.shorten_urls(urls)
-        await message.edit("Here Your link\n{}".format(shortlink[0]),
-                                                       disable_web_page_preview=True
-                                                       )
 
 
 @app.on_message(Filters.command("expand", Command) & Filters.me)
