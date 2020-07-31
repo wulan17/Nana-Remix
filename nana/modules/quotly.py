61a34d89fac63a794e5e8294a60cd1d7e61ceed4
pokurt <aman@virtual.mv>
2020-08-01 00:28:08 +0500

    hoping issues with quotly gets a fix

--- nana/modules/quotly.py
+++ nana/modules/quotly.py
@@ -18,12 +18,10 @@ __Reply To Message Text To Create Quote Sticker.__
 @app.on_message(Filters.user(AdminSettings) & Filters.command("q", Command))
 async def q_maker(_client, message):
     if not message.reply_to_message:
-        await msg("**Reply to any users text message**")
+        await msg(message, text="**Reply to any users text message**")
         return
-    await msg("**Making a Quote**")
     await message.reply_to_message.forward("@QuotLyBot")
     is_sticker = False
-    progress = 0
     while not is_sticker:
         try:
             ms_g = await app.get_history("@QuotLyBot", 1)
@@ -33,13 +31,11 @@ async def q_maker(_client, message):
         except Exception as e:
             print(e)
             await sleep(0.5)
-            progress += random.randint(0, 10)
             try:
-                await message.edit("**Making a Quote**\n**Processing** ```{}%```".format(progress))
+                await msg(message, text="**Making a Quote**")
             except Exception as e:
                 print(e)
-                await message.edit("**ERROR ⚠**")
-    await message.edit("**Completed!**")
+    await msg(message, text="**Completed!**")
     msg_id = ms_g[0]["message_id"]
     await message.delete()
     await app.forward_messages(message.chat.id, "@QuotLyBot", msg_id, as_copy=True)
--- nana/modules/updater.py
+++ nana/modules/updater.py
@@ -1,70 +1,174 @@
 import os
 import shutil
+import sys
 
-import requests
+from git import Repo
+from git.exc import InvalidGitRepositoryError, GitCommandError, NoSuchPathError
 from pyrogram import Filters
 
-from nana import app, Command, AdminSettings
+from nana import app, Command, OFFICIAL_BRANCH, REPOSITORY, HEROKU_API, AdminSettings
+from nana.__main__ import restart_all, except_hook
+from nana.assistant.updater import update_changelog
 from nana.helpers.PyroHelpers import msg
 
-__MODULE__ = "Uploader"
+__MODULE__ = "Updater"
 __HELP__ = """
-Reupload URL image to telegram without save it first.
+You want to update latest version?
+Easy, just type like bellow
 
-──「 **Upload image** 」──
--> `pic (url)`
-Upload image from URL
+──「 **Check update** 」──
+-> `update`
+Only check update if avaiable
 
-──「 **Upload sticker** 」──
--> `stk (url)`
-Upload image and convert to sticker, please note image from telegraph will result bug (telegram bugs)
+──「 **Update bot** 」──
+-> `update now`
+Update your bot to latest version
 """
 
 
-@app.on_message(Filters.user(AdminSettings) & Filters.command("pic", Command))
-async def PictureUploader(client, message):
-    if len(message.text.split()) == 1:
-        await msg(message, text="Usage: `.pic <url>`")
-        return
-    photo = message.text.split(None, 1)[1]
-    await message.delete()
-    if "http" in photo:
-        r = requests.get(photo, stream=True)
-        with open("nana/cache/pic.png", "wb") as stk:
-            shutil.copyfileobj(r.raw, stk)
-        if message.reply_to_message:
-            await client.send_photo(message.chat.id, "nana/cache/pic.png",
-                                    reply_to_message_id=message.reply_to_message.message_id)
-        else:
-            await client.send_photo(message.chat.id, "nana/cache/pic.png")
-        os.remove("nana/cache/pic.png")
-    else:
-        if message.reply_to_message:
-            await client.send_photo(message.chat.id, photo, "",
-                                    reply_to_message_id=message.reply_to_message.message_id)
-        else:
-            await client.send_photo(message.chat.id, photo, "")
+async def gen_chlog(repo, diff):
+    changelog = ""
+    d_form = "%H:%M - %d/%m/%y"
+    for cl in repo.iter_commits(diff):
+        changelog += f'• [{cl.committed_datetime.strftime(d_form)}]: {cl.summary} <{cl.author}>\n'
+    return changelog
 
 
-@app.on_message(Filters.user(AdminSettings) & Filters.command("stk", Command))
-async def StickerUploader(client, message):
-    if len(message.text.split()) == 1:
-        await msg(message, text="Usage: `.stk <url>`")
+async def initial_git(repo):
+    isexist = os.path.exists('nana-old')
+    if isexist:
+        shutil.rmtree('nana-old')
+    os.mkdir('nana-old')
+    os.rename('nana', 'nana-old/nana')
+    os.rename('.gitignore', 'nana-old/.gitignore')
+    os.rename('LICENSE', 'nana-old/LICENSE')
+    os.rename('README.md', 'nana-old/README.md')
+    os.rename('requirements.txt', 'nana-old/requirements.txt')
+    os.rename('Procfile', 'nana-old/Procfile')
+    os.rename('runtime.txt', 'nana-old/runtime.txt')
+    update = repo.create_remote('master', REPOSITORY)
+    update.pull('master')
+    os.rename('nana-old/nana/config.py', 'nana/config.py')
+    shutil.rmtree('nana/session/')
+    os.rename('nana-old/nana/session/', 'nana/session/')
+
+
+@app.on_message(Filters.me & Filters.command("update", Command))
+async def updater(client, message):
+    await msg(message, text="__Checking update...__")
+    initial = False
+    try:
+        repo = Repo()
+    except NoSuchPathError as error:
+        await msg(message, text=f"**Update failed!**\n\nError:\n`directory {error} is not found`")
         return
-    photo = message.text.split(None, 1)[1]
-    await message.delete()
-    if "http" in photo:
-        r = requests.get(photo, stream=True)
-        with open("nana/cache/stiker.png", "wb") as stk:
-            shutil.copyfileobj(r.raw, stk)
-        if message.reply_to_message:
-            await client.send_sticker(message.chat.id, "nana/cache/stiker.png",
-                                      reply_to_message_id=message.reply_to_message.message_id)
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        initial = True
+    except GitCommandError as error:
+        await msg(message, text=f'**Update failed!**\n\nError:\n`{error}`')
+        return
+
+    if initial:
+        if len(message.text.split()) != 2:
+            await msg(message, text='Your git workdir is missing!\nBut i can repair and take new latest update for you.\nJust do `update '
+                'now` to repair and take update!')
+            return
+        elif len(message.text.split()) == 2 and message.text.split()[1] == "now":
+            try:
+                await initial_git(repo)
+            except Exception as err:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                await msg(message, text=f'**Error:**\n{err}')
+                await except_hook(exc_type, exc_obj, exc_tb)
+                return
+            await msg(message, text='Successfully Updated!\nBot is restarting...')
+            await update_changelog(
+                "-> **WARNING**: Bot has been created a new git and sync to latest version, your old files is in nana-old")
+            await restart_all()
+            return
+
+    brname = repo.active_branch.name
+    if brname not in OFFICIAL_BRANCH:
+        await msg(message, text=f'**[UPDATER]:** Looks like you are using your own custom branch ({brname}). in that case, Updater is unable to identify which branch is to be merged. please checkout to any official branch')
+        return
+    try:
+        repo.create_remote('upstream', REPOSITORY)
+    except BaseException:
+        pass
+
+    upstream = repo.remote('upstream')
+    upstream.fetch(brname)
+    try:
+        changelog = await gen_chlog(repo, f'HEAD..upstream/{brname}')
+    except Exception as err:
+        if "fatal: bad revision" in str(err):
+            try:
+                await initial_git(repo)
+            except Exception as err:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                await msg(message, text=f'**Error:**\n{err}')
+                await except_hook(exc_type, exc_obj, exc_tb)
+                return
+            await msg(message, text='Successfully Updated!\nBot is restarting...')
+            await update_changelog("-> **WARNING**: Bot has been created a new git and sync to latest version, your old files is in nana-old")
+            await restart_all()
+            return
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        await msg(message, text='An error has accured!\nPlease see your Assistant for more information!')
+        await except_hook(exc_type, exc_obj, exc_tb)
+        return
+
+    if not changelog:
+        await msg(message, text=f'Nana is up-to-date with branch **{brname}**\n')
+        return
+
+    if len(message.text.split()) != 2:
+        changelog_str = f'To update latest changelog, do\n-> `update now`\n\n**New UPDATE available for [{brname}]:\n' \
+                        f'\nCHANGELOG:**\n`{changelog}` '
+        if len(changelog_str) > 4096:
+            await msg(message, text="`Changelog is too big, view the file to see it.`")
+            file = open("nana/cache/output.txt", "w+")
+            file.write(changelog_str)
+            file.close()
+            await client.send_document(message.chat.id, "nana/cache/output.txt", reply_to_message_id=message.message_id,
+                                       caption="`Changelog file`")
+            os.remove("nana/cache/output.txt")
         else:
-            await client.send_sticker(message.chat.id, "nana/cache/stiker.png")
-        os.remove("nana/cache/stiker.png")
+            await msg(message, text=changelog_str)
+        return
+    elif len(message.text.split()) == 2 and message.text.split()[1] == "now":
+        await msg(message, text='`New update found, updating...`')
+        if HEROKU_API is not None:
+            import heroku3
+            heroku = heroku3.from_key(HEROKU_API)
+            heroku_applications = heroku.apps()
+            if len(heroku_applications) >= 1:
+                heroku_app = heroku_applications[0]
+                heroku_git_url = heroku_app.git_url.replace(
+                    "https://",
+                    "https://api:" + HEROKU_API + "@"
+                )
+                if "heroku" in repo.remotes:
+                    remote = repo.remote("heroku")
+                    remote.set_url(heroku_git_url)
+                else:
+                    remote = repo.create_remote("heroku", heroku_git_url)
+                remote.push(refspec="HEAD:refs/heads/master")
+            else:
+                await message.reply("no heroku application found, but a key given? 😕 ")
+            await msg(message, text="Build Unsuccess, Check heroku build log for more detail")
+            return
+        try:
+            upstream.pull(brname)
+            await msg(message, text='Successfully Updated!\nBot is restarting...')
+        except GitCommandError:
+            repo.git.reset('--hard')
+            repo.git.clean('-fd', 'nana/modules/')
+            repo.git.clean('-fd', 'nana/assistant/')
+            repo.git.clean('-fd', 'nana/helpers/')
+            await msg(message, text='Successfully Updated!\nBot is restarting...')
+        await update_changelog(changelog)
+        await restart_all()
     else:
-        if message.reply_to_message:
-            await client.send_sticker(message.chat.id, photo, reply_to_message_id=message.reply_to_message.message_id)
-        else:
-            await client.send_sticker(message.chat.id, photo)
+        await msg(message, text="Usage:\n-> `update` to check update\n-> `update now` to update latest commits\nFor more information ")
