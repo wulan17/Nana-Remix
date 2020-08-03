d1328d63bca8a64a9593b3525e25d98ade9a7fe7
pokurt <aman@virtual.mv>
2020-08-03 21:48:54 +0500

    Revert "fixed issues with inline"
    This reverts commit da5754831fbdffb19b90f070389f8c9737a64fe9.
    
    Signed-off-by: Poki <pokurt@git-gram.gq>

--- nana/modules/speedtest.py
+++ nana/modules/speedtest.py
@@ -23,14 +23,14 @@ async def speedtestxyz_callback(client, query):
         replymsg = 'SpeedTest Results:'
         if query.data == 'speedtest_image':
             speedtest_image = speed.results.share()
-            await app.send_photo(query.id,
+            await app.send_photo(query.chat.id,
                 photo=speedtest_image, caption=replymsg)
             msg.delete()
 
         elif query.data == 'speedtest_text':
             result = speed.results.dict()
             replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
-            await query.message.edit_text(
+            await query.edit(
                 replymsg, parse_mode="markdown")
     else:
         await client.answer_callback_query(query.id, "No, you are not allowed to do this", show_alert=False)
