a1f5b06509fb56956da304b0a2a8f07498dc862b
pokurt <aman@virtual.mv>
2020-08-04 15:45:33 +0500

    Fixed most of the issues
    Added New way of getting a speed test
    Downgraded pyrogram version to 0.17.0
    
    Signed-off-by: Poki <pokurt@git-gram.gq>

--- nana/modules/speedtest.py
+++ nana/modules/speedtest.py
@@ -1,5 +1,6 @@
 from pyrogram import Filters
-from nana import setbot, app, AdminSettings
+from nana import setbot, AdminSettings, BotUsername, app, Command
+from nana.helpers.PyroHelpers import ReplyCheck
 import speedtest
 import re
 
@@ -9,27 +10,47 @@ def speedtest_callback(_, query):
 
 speedtest_create = Filters.create(speedtest_callback)
 
-def convert(speed):
-    return round(int(speed) / 1048576, 2)
+def speed_convert(size):
+    """Hi human, you can't read bytes?"""
+    power = 2 ** 10
+    zero = 0
+    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"
 
 @setbot.on_callback_query(speedtest_create)
 async def speedtestxyz_callback(client, query):
     if query.from_user.id in AdminSettings:
-        msg = await setbot.edit_inline_text(query.inline_message_id,'Runing a speedtest....')
+        await setbot.edit_inline_text(query.inline_message_id,'Runing a speedtest....')
         speed = speedtest.Speedtest()
         speed.get_best_server()
         speed.download()
         speed.upload()
-        replymsg = 'SpeedTest Results:'
+        replymsg = '**SpeedTest Results:**'
         if query.data == 'speedtest_image':
             speedtest_image = speed.results.share()
-            await app.send_photo(query.id,
-                photo=speedtest_image, caption=replymsg)
-            msg.delete()
+            replym = f"**[SpeedTest Results:]({speedtest_image})**"
+            await setbot.edit_inline_text(query.inline_message_id, replym, parse_mode="markdown")
 
         elif query.data == 'speedtest_text':
             result = speed.results.dict()
-            replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
+            replymsg += f"\n - **ISP:** `{result['client']['isp']}`"
+            replymsg += f"\n - **Download:** `{speed_convert(result['download'])}Mb/s`"
+            replymsg += f"\n - **Upload:** `{speed_convert(result['upload'])}Mb/s`"
+            replymsg += f"\n - **Ping:** `{result['ping']}`"
             await setbot.edit_inline_text(query.inline_message_id, replymsg, parse_mode="markdown")
     else:
         await client.answer_callback_query(query.id, "No, you are not allowed to do this", show_alert=False)
+
+
+@app.on_message(Filters.user(AdminSettings) & Filters.command("speedtest", Command))
+async def google_search(client, message):
+    x = await client.get_inline_bot_results(f"{BotUsername}", f"speedtest")
+    await message.delete()
+    await client.send_inline_bot_result(chat_id=message.chat.id,
+                                        query_id=x.query_id,
+                                        result_id=x.results[0].id,
+                                        reply_to_message_id=ReplyCheck(message),
+                                        hide_via=True)
