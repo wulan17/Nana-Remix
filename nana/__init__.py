5a2f2a7ed9d23a2b7013dfd9302d68ce1e1749ae
pokurt <aman@virtual.mv>
2020-08-22 17:35:08 +0500

    test mode

--- nana/__init__.py
+++ nana/__init__.py
@@ -234,7 +234,7 @@ BASE = declarative_base()
 SESSION = mulaisql()
 
 setbot = Client(BOT_SESSION, api_id=api_id, api_hash=api_hash, bot_token=ASSISTANT_BOT_TOKEN, workers=ASSISTANT_WORKER,
-                test_mode=TEST_MODE)
+                test_mode=True)
 
 app = Client(APP_SESSION, api_id=api_id, api_hash=api_hash, app_version=app_version, device_model=device_model,
-             system_version=system_version, lang_code=lang_code, workers=NANA_WORKER, test_mode=TEST_MODE)
+             system_version=system_version, lang_code=lang_code, workers=NANA_WORKER, test_mode=True)
